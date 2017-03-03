365c4bd9567339f9b13fa1068001e52a5a86df45
wjtribbitt <wjtribbitt@gmail.com>
2017-03-03 09:37:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,9 +28,9 @@ function respond() {
     postMessage("https://i.groupme.com/640x364.jpeg.f8a0e216bf7e42ed9a86b3ae23cc2ff1.large");
     this.res.end();
   } 
-  else if(request.text && botRegexStop.test(request.text)) {
+  else if(request.text && botRegexSchedule.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("Stop appropriating black culture");
+    postMessage("http://daddyleagues.com/tmi/schedules");
     this.res.end();
   } 
   else if(request.text && botRegexProp.test(request.text)) {
